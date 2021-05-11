166d6e8df4d3782a1665a0528d128ef3a41f29ed
nazar1ous <kuspys@ucu.edu.ua>
2021-05-11 20:51:59 +0300

    force

---
+++ cmake-build-debug/CMakeFiles/parallel_neural_net_lib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nazariikuspys/UCU/AKS/parallel-lib-neur-net/src/main.cpp" "/home/nazariikuspys/UCU/AKS/parallel-lib-neur-net/cmake-build-debug/CMakeFiles/parallel_neural_net_lib.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc/eigen-3.3.9"
+  "../inc"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nazariikuspys/UCU/AKS/parallel-lib-neur-net/cmake-build-debug/CMakeFiles/liba.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/parallel_neural_net_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_neural_net_lib.dir/src/main.cpp.o"
+  "parallel_neural_net_lib"
+  "parallel_neural_net_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel_neural_net_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
